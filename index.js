2f690c128e6fb81688b7beb4c6ab8ba67ab20ce0
Scott Atkins <satkins16@gmail.com>
2017-11-28 13:38:12 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function displayRepositories() {
       <li>
         <h2>${repo.name}</h2>
         <a href="${repo.html_url}">${repo.html_url}</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
     `)
   })
 }
