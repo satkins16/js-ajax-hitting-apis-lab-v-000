490e0d6431c94271ed5925c83b146b1ea2344387
Scott Atkins <satkins16@gmail.com>
2017-11-28 13:24:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function getRepositories() {
   const name = document.getElementById("username").value
+  const uri = rootURL + "/users/" + name + "/repos"
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener("load", displayRepositories)
+  xhr.open("GET", uri)
+  xhr.send()
+  return false;
 }
